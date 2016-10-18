7d3cb5a1d0bc18569424c9fae1c7d2eef3db6178
Hpar <raph@hpar.fr>
2016-10-18 12:50:38 +0200

    Revert "Feat/refact err handl"

--- roulier/carriers/laposte/laposte.py
+++ roulier/carriers/laposte/laposte.py
@@ -21,7 +21,7 @@ class Laposte(Carrier):
         """Run an action with data against Laposte WS."""
         request = self.encoder.encode(data, action)
         response = self.ws.send(request)
-        if "payload" not in response:
+        if not response['payload']:
             return response
         parts = self.ws.get_parts(response['response'])
         return self.decoder.decode(response, parts)
