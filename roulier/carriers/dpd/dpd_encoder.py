ff8322d8ac0829329d3d0cee2fd9cd665c369d71
hparfr <raph@hpar.fr>
2016-10-27 15:01:15 +0200

    don't leak zpl format to ws

--- roulier/carriers/dpd/dpd_encoder.py
+++ roulier/carriers/dpd/dpd_encoder.py
@@ -29,6 +29,10 @@ class DpdEncoder(Encoder):
             .strftime('%d/%M/%Y')
         )
 
+        if data['service']['labelFormat'] == 'ZPL':
+            # WS doesn't handle zpl yet, we convert it later
+            data['service']['labelFormat'] = 'PNG'
+
         env = Environment(
             loader=PackageLoader('roulier', '/carriers/dpd/templates'),
             extensions=['jinja2.ext.with_'])
