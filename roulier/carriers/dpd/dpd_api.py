1d37eb7a6c0a44d3ee39b51b23c2d662d621362d
David Beal <david.beal@akretion.com>
2019-05-17 15:11:58 +0200

    FIX comment description key in schema

--- roulier/carriers/dpd/dpd_api.py
+++ roulier/carriers/dpd/dpd_api.py
@@ -30,13 +30,16 @@ class DpdApi(Api):
         schema['labelFormat'].update({'required': True, 'empty': False})
         schema['agencyId'].update({
             'required': True, 'empty': False,
-            'description': 'Agency code int(3)'})
+            # 'description': 'Agency code int(3)'
+            })
         schema['customerCountry'] = {
             'required': True, 'empty': False,
-            'description': 'Customer country code (France = 250) int(3)'}
+            # 'description': 'Customer country code (France = 250) int(3)'
+            }
         schema['customerId'].update({
             'required': True, 'empty': False,
-            'description': 'Customer number int(6)'})
+            # 'description': 'Customer number int(6)'
+            })
         schema['shippingDate'].update({'required': False, 'empty': True})
 
         # mettre ça ensemble ?
@@ -47,7 +50,7 @@ class DpdApi(Api):
             'empty': False,
             'required': True,
             'default': DPD_PRODUCTS[0],
-            'description': 'Type de produit',
+            # 'description': 'Type de produit',
             'allowed': DPD_PRODUCTS
         })
 
@@ -55,15 +58,15 @@ class DpdApi(Api):
             'default': '',
             'empty': True,
             'required': False,
-            'description': 'Drop-off Location id (Relais Colis)'
+            # 'description': 'Drop-off Location id (Relais Colis)'
         }
 
         return schema
 
     def _address(self):
         schema = super(DpdApi, self)._address()
-        schema['street2']['description'] = (
-            "N/A for DPD. It will be appended to street1")
+        # schema['street2']['description'] = (
+        #     "N/A for DPD. It will be appended to street1")
         schema['country'].update({'required': True, 'empty': False})
         schema['zip'].update({'required': True, 'empty': False})
         schema['city'].update({'required': True, 'empty': False})
@@ -71,13 +74,13 @@ class DpdApi(Api):
 
     def _to_address(self):
         schema = super(DpdApi, self)._to_address()
-        schema['firstName'] = {'default': '', 'description': """First name""",
-            'coerce': 'accents'}
-        schema['door1'] = {'default': '', 'description': """Door code 1"""}
-        schema['door2'] = {'default': '', 'description': """Door code 2"""}
-        schema['intercom'] = {'default': '', 'description': """Intercom"""}
+        # , 'description': """First name""",
+        schema['firstName'] = {'default': '', 'coerce': 'accents'}
+        schema['door1'] = {'default': ''}  # 'description': """Door code 1"""
+        schema['door2'] = {'default': ''}  # 'description': """Door code 2"""}
+        schema['intercom'] = {'default': ''}  # 'description': """Intercom"""}
         for field in ['city', 'company', 'name', 'street1', 'street2']:
-            schema[field].update({ 'coerce': 'accents'})
+            schema[field].update({'coerce': 'accents'})
         return schema
 
     def _from_address(self):
--- roulier/carriers/laposte/laposte_api.py
+++ roulier/carriers/laposte/laposte_api.py
@@ -23,34 +23,43 @@ class LaposteApi(Api):
         schema['labelFormat'].update({'required': True, 'empty': False})
         schema['product'].update({'required': True, 'empty': False})
         schema['pickupLocationId'] = {
-            'default': '', 'description':
-            """Si productCode = A2P, BPR, ACP, CDI, CMT, BDP. "
-            "Identifiant du point de retrait "
-            "(dans le cas d’une livraison Colissimo hors domicile)"""}
+            'default': '',
+            # 'description': """Si productCode = A2P, BPR, ACP, CDI, CMT, BDP. "
+            # "Identifiant du point de retrait "
+            # "(dans le cas d’une livraison Colissimo hors domicile)"""
+            }
         schema['totalAmount'] = {
-            'default': '', 'description': 'Needed for cn23'}
+            'default': '',
+            # 'description': 'Needed for cn23'
+            }
         schema['orderNumber'] = {'default': ''}
         schema['commercialName'] = {
-            'default': '', 'description':
-            """Obligatoire pour les produits DOM, DOS, BPR, A2P. "
-            "Nom commercial du chargeur qui sera affiché dans "
-            "les notifications par email aux destinataires des colis"""}
+            'default': '',
+            # 'description':
+            # """Obligatoire pour les produits DOM, DOS, BPR, A2P. "
+            # "Nom commercial du chargeur qui sera affiché dans "
+            # "les notifications par email aux destinataires des colis"""
+            }
         schema['returnTypeChoice'] = {
-            'default': '', 'description':
-            """Obligatoire pour certains colis à l’international, "
-            "selon les zones tarifaires applicables. Indique si "
-            "le colis doit être retourné à l’expéditeur en cas de "
-            "non distribution du colis"""}
+            'default': '',
+            # 'description':
+            # """Obligatoire pour certains colis à l’international, "
+            # "selon les zones tarifaires applicables. Indique si "
+            # "le colis doit être retourné à l’expéditeur en cas de "
+            # "non distribution du colis"""
+            }
         schema['returnType'] = {
-            'default': '', 'description':
-            """Utilisé pour le Colissimo Retour uniquement. "
-            "Définit le mode de transmission de l’étiquette"""}
-        schema['reference1'].update({
-            'description': """Référence expediteur ('Réf client')"""
-        })
-        schema['reference2'].update({
-            'description': """Référence destinataire ('Réf destinataire')"""
-        })
+            'default': '',
+            # 'description':
+            # """Utilisé pour le Colissimo Retour uniquement. "
+            # "Définit le mode de transmission de l’étiquette"""
+            }
+        # schema['reference1'].update({
+        #     'description': """Référence expediteur ('Réf client')"""
+        # })
+        # schema['reference2'].update({
+        #     'description': """Référence destinataire ('Réf destinataire')"""
+        # })
         return schema
 
     def _address(self):
@@ -60,20 +69,28 @@ class LaposteApi(Api):
         schema['city'].update({'required': True, 'empty': False})
         schema['street0'] = {
             'required': False, 'empty': True,
-            'description': """Entrée, bâtiment, immeuble, résidence. """
-                           """Non utilisé pour la Belgique."""}
+            # 'description': """Entrée, bâtiment, immeuble, résidence. """
+            #                """Non utilisé pour la Belgique."""
+            }
         schema['street2'].update({
-            'default': '', 'description': 'Etage, couloir, escalier, appart.'})
+            'default': ''
+            # , 'description': 'Etage, couloir, escalier, appart.'
+            })
         schema['street1'].update({
             'required': True, 'empty': False,
-            'description': 'Numéro et libellé de voie. Ex : 5 rue du Bellay'})
+            # 'description': 'Numéro et libellé de voie. Ex : 5 rue du Bellay'
+            })
         schema['street3'] = {
             'default': '',
-            'description': """Lieu dit ou autre mention. """
-                           """Non utilisé pour la Belgique."""}
-        schema['door1'] = {'default': '', 'description': """Code porte 1"""}
-        schema['door2'] = {'default': '', 'description': """Code porte 2"""}
-        schema['intercom'] = {'default': '', 'description': """Interphone"""}
+            # 'description': """Lieu dit ou autre mention. """
+            #                """Non utilisé pour la Belgique."""
+            }
+        # 'description': """Code porte 1"""
+        schema['door1'] = {'default': ''}
+        # 'description': """Code porte 2"""
+        schema['door2'] = {'default': ''}
+        # 'description': """Interphone"""
+        schema['intercom'] = {'default': ''}
 
         return schema
 
@@ -85,41 +102,49 @@ class LaposteApi(Api):
         schema = super(LaposteApi, self)._to_address()
         schema['firstName'] = {
             'default': '',
-            'description': """Prénom. Obligatoire pour So Colissimo"""}
+            }
+            # 'description': """Prénom. Obligatoire pour So Colissimo"""
         return schema
 
     def _parcel(self):
         schema = super(LaposteApi, self)._parcel()
         schema['nonMachinable'] = {
             'type': 'boolean', 'default': False,
-            'description': """Passer à true pour indiquer que le "
-            "format du colis est non standard"""}
+            # 'description': """Passer à true pour indiquer que le "
+            # "format du colis est non standard"""
+            }
         schema['instructions'] = {
-            'default': '', 'description': """Indications complémentaires "
-            "pour la livraison"""}
+            'default': '',
+            # 'description': """Indications complémentaires pour la livraison"""
+            }
         schema['insuranceValue'] = {
             'type': 'number', 'default': 0,
-            'description': """Valeur assurée. Max= 1500€ "
-            "Passer 1230 pour 12,30€ Cette valeur sera arrondie "
-            "à l’entier le plus proche (Ex : 12 euros si 1232 est envoyé) "
-            "Par défaut, renseigner « 0 » (zéro)"""}
+            # 'description': """Valeur assurée. Max= 1500€ "
+            # "Passer 1230 pour 12,30€ Cette valeur sera arrondie "
+            # "à l’entier le plus proche (Ex : 12 euros si 1232 est envoyé) "
+            # "Par défaut, renseigner « 0 » (zéro)"""
+            }
         schema['recommendationLevel'] = {
-            'default': '', 'description': """Niveau de recommandation "
-            "(cf. III.2) Peut valoir « R1 », ou « R2 », ou « R3 »"""}
+            'default': '',
+            # 'description': """Niveau de recommandation "
+            # "(cf. III.2) Peut valoir « R1 », ou « R2 », ou « R3 »"""
+            }
         schema['cod'] = {
             'type': 'boolean', 'default': False,
-            'description': """Passer à true si la livraison doit se "
-            "faire contre remboursement."""}
+            # 'description': """Passer à true si la livraison doit se faire contre remboursement."""
+            }
         schema['codAmount'] = {
             'type': 'number', 'default': 0,
-            'description': """Montant attendu lors de la "
-            "livraison contre remboursement. Par défaut, renseigner "
-            "« 0 » (zéro)"""}
+            # 'description': """Montant attendu lors de la "
+            # "livraison contre remboursement. Par défaut, renseigner "
+            # "« 0 » (zéro)"""
+            }
         schema['ftd'] = {
             'type': 'boolean', 'default': False,
-            'description': """Pour les envois vers "
-            "l’Outre-Mer uniquement Indique si le colis est franc de taxes "
-            "et de droits."""}
+            # 'description': """Pour les envois vers "
+            # "l’Outre-Mer uniquement Indique si le colis est franc de taxes "
+            # "et de droits."""
+            }
         return schema
 
     def _auth(self):
