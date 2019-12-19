0a8d9df63f727484a329408eff73df49ac6dabaa
hparfr <raph@hpar.fr>
2019-12-19 12:05:31 +0100

    Fix dpd_fr:api() and prepare support to other encoders

--- roulier/carriers/dpd_fr/dpd.py
+++ roulier/carriers/dpd_fr/dpd.py
@@ -24,16 +24,18 @@ class Dpd(Carrier):
         return method(self, None, api=True)
 
 
-    def get(self, data, action):
+    def get(self, data, action, api):
         """Run an action with data against Dpd WS."""
+        if api:
+            return self.encoder.api(action)
         request = self.encoder.encode(data, action)
         response = self.ws.send(request)
         return self.decoder.decode(response, request, action)
 
     # shortcuts
-    def get_label(self, data):
+    def get_label(self, data, api=False):
         """Genereate a createShipmentWithLabels."""
-        return self.get(data, 'createShipmentWithLabels')
+        return self.get(data, 'createShipmentWithLabels', api)
 
     ACTIONS = {
         'label': get_label,
