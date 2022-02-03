d7f11e264031c82d0d34493099b2aafcfdc0aa9a
mathieu <mathieu@pharmasimple.com>
2022-02-03 10:41:58 +0100

    [FIX] name, phone, mobile of ADDRESS_MAPPING

--- roulier/carriers/gls_fr/glsbox/encoder.py
+++ roulier/carriers/gls_fr/glsbox/encoder.py
@@ -35,15 +35,15 @@ ACCOUNT_MAPPING = {
 }
 ADDRESS_MAPPING = {
     "T860": "company",
-    "T8906": "name",
+    "T860": "name",
     "T863": "street1",
     "T861": "street2",
     "T862": "street3",
     "T330": "zip",
     "T864": "city",
     "T100": "country",
-    "T871": "phone",
-    "T1230": "mobile",
+    "T758": "phone",
+    "T759": "mobile",
     "T1229": "email",
 }
 
