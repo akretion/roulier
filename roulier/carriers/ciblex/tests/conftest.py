f86b698e67e03f7f8992c8fe961831e5a9777c9e
Florian Mounier <paradoxxx.zero@gmail.com>
2024-09-17 17:03:03 +0200

    Add ciblex transporter

---
+++ roulier/carriers/ciblex/tests/conftest.py
@@ -0,0 +1,6 @@
+# Copyright 2024 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from ....tests.conftest import *  # noqa
